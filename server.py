809db5a701798046f792ad1a188e985694d1e881
Richard Lopez <richard.lopez@columbia.edu>
2017-04-02 22:34:57 -0400

    removed test table and inserted person table so that server.py can run

--- server.py
+++ server.py
@@ -107,10 +107,10 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM test")
+  cursor = g.conn.execute("SELECT pname FROM person")
   names = []
   for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
+    names.append(result['pname'])  # can also be accessed using result[0]
   cursor.close()
 
   #
@@ -181,77 +181,33 @@ def competitions():
 
 @app.route('/userProfile', methods=['GET', 'POST'])
 def userProfile():
-  """
-  request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
-  # DEBUG: this is debugging code to see what request looks like
-  print request.args
-
-
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT pname FROM Person")
-  names = []
-  for result in cursor:
-    names.append(result[0])  # can also be accessed using result[0]
-  cursor.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(usersList = names)
+  users = g.conn.execute("SELECT pname FROM person").fetchall()
+  user = users[0][0]
+  if request.method == 'POST':
+    user = request.form['userDropdown']
+  person = g.conn.execute(text("SELECT * FROM person WHERE pname= :up "),up=userProfile).fetchone()
+  context = dict(users=users, person=person)
 
 
   return render_template("userProfile.html", **context)
 
 
 
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  g.conn.execute(text("INSERT INTO test(name) VALUES ( :nm )"),nm=name)
-  return redirect('/')
+# # Example of adding new data to the database
+# @app.route('/add', methods=['POST'])
+# def add():
+#   # name = request.form['name']
+#   # g.conn.execute(text("INSERT INTO test(name) VALUES ( :nm )"),nm=name)
+#   # return redirect('/')
 
-# Example of getting table data from the database
-@app.route('/getUserData', methods=['GET'])
-def getUserData():
-  #print request.args
-  #userProfile = "Jacob Fitzgerald"
-  userProfile = request.form['userDropDown']
-  userData = g.conn.execute(text("SELECT * FROM Person WHERE pname = :up "), up=userProfile)
-  return redirect('/userProfile')
+# # Example of getting table data from the database
+# @app.route('/getUserData', methods=['GET'])
+# def getUserData():
+#   #print request.args
+#   #userProfile = "Jacob Fitzgerald"
+#   userProfile = request.form['userDropDown']
+#   userData = g.conn.execute(text("SELECT * FROM Person WHERE pname = :up "), up=userProfile)
+#   return redirect('/userProfile')
 
 @app.route('/login')
 def login():
